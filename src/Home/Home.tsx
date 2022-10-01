cfe9371556e37188fadab7e26112e6705c5f69ec
Josan Johnata <josan.pontes@meliuz.com.br>
2022-10-01 20:24:38 -0300

    feat: Adiciona o menu principal com as seções

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import styles from './Home.module.css';
 import {
+  FolderOpen,
+  MediumLogo,
+  InstagramLogo,
+  DeviceMobile,
   ToggleLeft
 } from 'phosphor-react';
 
@@ -15,7 +19,24 @@ function Home() {
             <h1 className={styles.title}>Josan Johnata</h1>
             <p className={styles.subtitle}>Full Stack Developer</p>
           </header>
-          
+          <section className={styles.sectionMenu}>
+            <button>
+              Meus projetos
+              <FolderOpen size={32} className={styles.sectionProjects} />
+            </button>
+            <button>
+              Blog
+              <MediumLogo size={32} className={styles.sectionBlog} />
+            </button>
+            <button>
+              Redes sociais
+              <InstagramLogo size={32} className={styles.sectionSocialMedia} />
+            </button>
+            <button>
+              Contatos
+              <DeviceMobile size={32} className={styles.sectionContact} />
+            </button>
+          </section>
         </main>
       </>
   );
