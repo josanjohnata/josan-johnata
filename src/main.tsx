c0d2f7c147c32021302965d15fb559fca58ff333
Josan Johnata <josan.pontes@meliuz.com.br>
2022-10-02 18:28:36 -0300

    feat: Adiciona rotas para as paginas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './Home/Home'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 )
