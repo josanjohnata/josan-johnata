53a25abf5448c6ccad0986e93a0ec24434a8e7d8
Josan Johnata <josan.pontes@meliuz.com.br>
2022-12-03 14:53:45 -0300

    fix: Corrige icon page e excluir a rota my project

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './Home/Home';
-import { MyProjects } from './pages/MyProjects';
 import { SocialMedia } from './pages/SocialMedias';
 import { Contacts } from './pages/Contacts';
 
@@ -9,7 +8,6 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/my-projects" element={<MyProjects />} />
       <Route path="/social-medias" element={<SocialMedia />} />
       <Route path="/contacts" element={<Contacts />} />
     </Routes>
