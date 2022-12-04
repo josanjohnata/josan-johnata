ce488e5a6a062a82df3c43f3435d0b1d38a8219f
Josan Johnata <josan.pontes@meliuz.com.br>
2022-12-04 19:42:42 -0300

    feat: Adiciona darkTheme e lightTheme

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -14,8 +14,12 @@ import johnIcon from '../assets/john-icon.png';
 import styles from './Home.module.css';
 
 function Home() {
-  const [isLight, setIsLight] = useState(false);
+  const [isLight, setIsLight] = useState(true);
   const navigate = useNavigate();
+
+  const toggleTheme = () => {
+    setIsLight(!isLight);
+  };
 
   const redirectSocialMedias = () => {
     navigate('/social-medias', { replace: true });
@@ -27,24 +31,22 @@ function Home() {
 
   return (
       <>
-        <main>
+        <main className={isLight === true ? styles.lightTheme : styles.darkTheme}>
           <header className={styles.header}>
             <span
               className={styles.toggleButton}
-              onClick={() => {
-                setIsLight(!isLight);
-              }}  
+              onClick={() => toggleTheme()}
             >
-            {
-              isLight === true? <ToggleRight size={32} color='#4EA8DE' className={styles.toggleRight} /> :
-              <ToggleLeft size={32} color='#808080' className={styles.toggleLeft} />
-            }
+              {
+                isLight === true? <ToggleRight size={32} color='#808080' /> :
+                <ToggleLeft size={32} color='#4EA8DE' />
+              }
             </span>
             <img src={johnIcon} alt="icon programmer"/>
             <h1 className={styles.title}>Josan Johnata</h1>
             <p className={styles.subtitle}>Full Stack Developer</p>
           </header>
-          <section className={styles.sectionMenu}>
+          <section className={isLight === true ? styles.lightTheme : styles.sectionMenu}>
             <a href={'https://github.com/josanjohnata'} target="_blank">
               My projects
               <FolderOpen size={32} className={styles.sectionProjects} />
